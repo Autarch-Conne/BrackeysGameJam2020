d232d4c7c82f53b3e57215ae59236f56b275deaf
Autarch-Conne <53091795+Autarch-Conne@users.noreply.github.com>
2020-08-02 19:31:33 -0500

    Added Project
    Very epic

---
+++ Brackeys Game Jam 2020/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+	public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+void GroundChecker()
+{
+	RaycastHit hit;
+	float distance = 1f;
+	Vector3 dir = new Vector3(0, -1);
+
+	if(Physics.Raycast(transform.position, dir, out hit, distance))
+	{
+		isGrounded = true;
+	}
+	else
+	{
+		isGrounded = false;
+	}
+}}
---
+++ Brackeys Game Jam 2020/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public Transform playerRotate;
+    public float mouseSensitivity = 50f;
+    float xRotator = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXAxis = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseYAxis = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        
+        xRotator -= mouseYAxis;
+        xRotator = Mathf.Clamp(xRotator, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotator, 0f, 0f);
+        playerRotate.Rotate(Vector3.up * mouseXAxis);
+    }
+}
